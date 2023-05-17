23b6c5cb25cfdd918b451be928f6240874dbc28c
al1kek <133839243+al1kek@users.noreply.github.com>
2023-05-17 14:03:49 +0600

    Add files via upload

---
+++ site/Front/energo/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="logo">logo</div>
+            <nav>
+                <div class="nav__main">Главная</div>
+                <div class="nsv__speed">SpeedType</div>
+                <div class="nav_game">vocalTune</div>
+            </nav>
+            <div class="autorization">Войти</div>
+        </header>
+        <main>
+            <div class="wish">
+                <span class="title">Раскройте свой потерциал: Energy - гармонизируйте музыку и повышайте точность и скорость кодирования</span>
+                <div class="image"></div>
+                <span class="slogan">"Энергия знаний мастерства и успеха"</span>
+            </div>
+            <span class="energoTeam"></span>
+            <hr class="line">
+            <div class="goals">
+                <div class="functions">
+                    <div class="function__puncts">ОбразованиеИОбучение</div>
+                    <div class="function__puncts">ТестСкоростиПечати</div>
+                    <div class="function__puncts">РазвитиеНавыков</div>
+                    <div class="function__puncts">ИнструментыПродуктивности</div>
+                    <div class="function__puncts">ОпределительНот</div>
+                    <div class="function__puncts">МузыкальныйТюнер</div>
+                </div>
+                <span>
+                    Мы стремимся помочь людям достичь своего потенциала в различных областях, предлагая инновационные решения и обучающие материалы.
+                </span>
+            </div>
+            <div class="blocks">
+                <div class="blocks__function">
+                    <div class="block__function__puncts"><h1>Продуктивность</h1>Мощный ритм работы</div>
+                    <div class="block__function__puncts"><h1>Обучение</h1>Непрерывное образование</div>
+                    <div class="block__function__puncts"><h1>Развитие навыков</h1>Личностный рост и совершенствование</div>
+                </div>
+                <div class="blocks__energy">Energy
+                    <span class="bigE"></span>
+                </div>
+            </div>
+            <div class="prof">
+                <span class="prof__punct">Наша цель - помочь музыкантам достичь идеального звучания и точности в исполнении, предоставляя инструменты, такие как тюнер и идентификатор нот.</span>
+                <span class="prof__punct">Для программистов мы ставим перед собой задачу помочь им улучшить свои навыки и эффективность, предлагая тест на скорость печати и другие полезные инструменты.</span>
+
+            </div>
+            <div class="creator">
+                <div class="creator__punct">
+                    <div class="photo"></div>
+                    <p>ФРОНТЕНД</p>
+                    <span class="name">
+                        Ахетов Алибек
+                    </span>
+                </div>
+                <div class="creator__punct">
+                    <div class="photo"></div>
+                    <p>БЕКЕНД</p>
+                    <span class="name">
+                        Алиев Эльдар
+                    </span>
+                </div>
+                <div class="creator__punct">
+                    <div class="photo"></div>
+                    <p>Дизайнер</p>
+                    <span class="name">
+                        Есмурат Асет
+                    </span>
+                </div>
+                <div class="creator__punct">
+                    <div class="photo"></div>
+                    <p>Копирайтер</p>
+                    <span class="name">
+                        ChatGPT
+                    </span>
+                </div>
+            </div>
+        </main>
+        <footer></footer>
+    </div>
+</body>
+</html>
